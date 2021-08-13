ce0bf95e5f657a2f72f147ec0279d39ff9a0905b
u-ever <uever.martins@gmail.com>
2021-08-13 01:20:55 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,76 @@
+var app = new function() {
+    this.el = document.getElementById('tasks');
+    this.tasks=[]
+
+    //Exibe (Lê/Read) a To Do List na tela.
+    this.FetchAll = function(){
+        var data ='';
+        console.log(tasks)
+
+        if(this.tasks.length>0){
+            for(i=0;i<this.tasks.length;i++){
+                data+='<tr>';
+                data+='<td>'+(i+1)+'. '+this.tasks[i]+'</td>';
+                data+='<td><button onclick="app.Edit('+i+')" class="btn btn-warning">Edit</button></td>';
+                data+='<td><button onclick="app.Delete('+i+')" class="btn btn-danger">Delete</button></td>';
+                data+='</tr>'
+            }
+        }
+        this.Count(this.tasks.length);
+        return this.el.innerHTML = data
+    };
+
+    //Adiciona (Cria/Create) um item a lista.
+    this.Add = function() {
+        el = document.getElementById('add-todo');
+        var task = el.value;
+        if(task){
+            this.tasks.push(task.trim());
+            el.value='';
+            this.FetchAll();
+        }
+    };
+
+    //Modifica (Edita/Edit) um item da lista.
+    this.Edit = function(item) {
+        el = document.getElementById('edit-todo');
+        el.value = this.tasks[item]
+        document.getElementById('edit-box').style.display = 'block'
+        self=this;
+
+        document.getElementById('save-edit').onsubmit = function(){
+            var task = el.value;
+            if(task){
+                self.tasks.splice(item, 1, task.trim())
+                self.FetchAll();
+                CloseInput();
+            }
+        }
+    };
+
+    //Exclui (Apaga/Delete)
+    this.Delete = function(item) {
+        this.tasks.splice(item,1)
+        this.FetchAll();
+    };
+
+    this.Count = function(data) {
+        var el = document.getElementById('counter');
+        var name = 'Tasks';
+        if(data){
+            if(data == 1){
+                name = 'Task';
+            }
+            el.innerHTML = data+' '+name;
+        }else{
+            el.innerHTML = "No "+ name;
+        }
+    };
+}
+
+app.FetchAll();
+
+//Esconde o input de edição
+function CloseInput() {
+    document.getElementById('edit-box').style.display= 'none';
+}
